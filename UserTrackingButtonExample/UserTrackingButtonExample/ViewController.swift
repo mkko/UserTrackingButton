8b03c6fe04c27490e3957e8f7b1fb68951447a33
Mikko Välimäki <mkko1373@gmail.com>
2017-09-23 10:02:44 +0200

    Update the example

--- UserTrackingButtonExample/UserTrackingButtonExample/ViewController.swift
+++ UserTrackingButtonExample/UserTrackingButtonExample/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
         locationManager.requestWhenInUseAuthorization()
         
         toolbar.setItems([
-            UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
             MKUserTrackingBarButtonItem(mapView: self.mapView)
             ], animated: false)
     }
@@ -43,11 +43,11 @@ class ViewController: UIViewController {
 
 extension ViewController: MKMapViewDelegate {
     
-    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         userTrackingButton.updateStateAnimated(true)
     }
     
-    func mapView(mapView: MKMapView, didChangeUserTrackingMode mode: MKUserTrackingMode, animated: Bool) {
+    func mapView(_ mapView: MKMapView, didChange mode: MKUserTrackingMode, animated: Bool) {
         userTrackingButton.updateStateAnimated(true)
     }
 }
