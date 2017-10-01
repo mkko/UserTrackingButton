e87562c12cbeb15abb0959dea76ceab248ec901f
Mikko Välimäki <mkko1373@gmail.com>
2017-10-01 16:34:47 +0200

    One attempt to add manual tracking to map view

--- UserTrackingButton/Tracker.swift
+++ UserTrackingButton/Tracker.swift
@@ -17,27 +17,74 @@ internal enum TrackerState {
     case trackingLocationWithHeading
 }
 
+extension MKMapView {
+
+    func currentMetersPerPoint() -> Double {
+        // The documentation on latitudeDelta states that "one degree
+        // of latitude is always approximately 111 kilometers"
+        // TODO: Consider also rotation?
+        let visibleSpanInMeters = self.region.span.latitudeDelta * 111000
+        return visibleSpanInMeters / Double(self.bounds.size.width)
+    }
+}
+
 public class Tracker: NSObject {
 
     var state = TrackerState.initial
 
     let mapView: MKMapView
 
+    let timer: DispatchSourceTimer
+
     var trackedAnnotation: MKAnnotation? {
         didSet {
             if let o = trackedAnnotation as? NSObject {
-                o.observe
+                timer.resume()
+                o.addObserver(self, forKeyPath: "coordinate", options: .new, context: nil)
+            } else {
+                timer.suspend()
             }
         }
+    }
+
+    private var zoomOnFollow = false
+
+    public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        //print("observeValue: \(keyPath)")
+        if let a = self.trackedAnnotation {
+            //let currentZoomScale: MKZoomScale = Double(mapView.bounds.size.width) / mapView.visibleMapRect.size.width
+
+            //self.mapView.setCenter(a.coordinate, animated: true)
+            //print("\(self.mapView.currentMetersPerPoint())")
+            print("\(self.mapView.visibleMapRect.size)")
+        }
+
     }
 
     private let serialQueue = DispatchQueue(label: "com.mikkovalimaki.UserTrackingButton", attributes: []);
 
     public init(mapView: MKMapView) {
         self.mapView = mapView
+
+        timer = DispatchSource.makeTimerSource(queue: DispatchQueue.main)
+        timer.schedule(deadline: .now(), repeating: .milliseconds(100), leeway: .seconds(1))
+        super.init()
+
+        timer.setEventHandler(handler: {
+            if let a = self.trackedAnnotation {
+                if self.zoomOnFollow {
+                    self.zoomOnFollow = false
+                    let region = MKCoordinateRegionMakeWithDistance(a.coordinate, 1000, 1000)
+                    self.mapView.setRegion(region, animated: false)
+                } else {
+                    self.mapView.setCenter(a.coordinate, animated: false)
+                }
+            }
+        })
     }
 
     public func follow(_ annotation: MKAnnotation) {
+        self.zoomOnFollow = true
         self.trackedAnnotation = annotation
     }
 
